5268d72bd46cc929e3cdbd8165227a898feef7b6
Okigbazi Victor Oluwabukunayomi <okigbazivictor@gmail.com>
2026-05-21 17:12:59 +0100

    i have created all of we would use for the project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+// function App() {
+//   return (
+//     // <div className="bg-black text-white p-10 text-4xl">
+//     //   Tailwind is working
+//     // </div>
+   
+//   );
+// }
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-5xl text-red-500">
+        Tailwind Working
+      </h1>
+    </div>
+  );
+}
+
+export default App;;
+
+
+// export default App
