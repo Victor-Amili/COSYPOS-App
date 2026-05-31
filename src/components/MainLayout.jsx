d5b51b11b6d34ece93dfb46b25e45ff12827f257
James Emem <jamesemem123@gmail.com>
2026-05-31 05:22:38 -0700

    Add mobile responsiveness to navbar and sidebar

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,18 +1,39 @@
+import { useState } from "react"
 import Sidebar from "./Sidebar"
 import Navbar from "./Navbar"
 import { Outlet } from "react-router-dom"
 
 function MainLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
   return (
     <div className="flex min-h-screen bg-black text-white">
-      <Sidebar />
 
-      <div className="flex flex-col flex-1">
-        <Navbar />
+      {/* Sidebar */}
+      <Sidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+      />
 
+      {/* Mobile Overlay */}
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black/50 md:hidden z-40"
+          onClick={() => setSidebarOpen(false)}
+        />
+      )}
+
+      {/* Main Content Area */}
+      <div className="flex flex-col flex-1 w-full">
+
+        {/* Navbar (controls sidebar) */}
+        <Navbar setSidebarOpen={setSidebarOpen} />
+
+        {/* Page Content */}
         <main className="flex-1 p-6">
           <Outlet />
         </main>
+
       </div>
     </div>
   )
