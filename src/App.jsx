dc64445a0e696acfe5cd4649820df93c70e1ad6b
Seelva <oluebubeonah67@gmail.com>
2026-06-12 01:52:21 +0100

    added reports page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom"
 import MainLayout from "./components/MainLayout"
 import Dashboard from "./pages/Dashboard"
+import ReservationReport from "./pages/ReservationReport"
+import RevenueReport from "./pages/RevenueReport"
+import StaffReport from "./pages/StaffReport"
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-
         <Route path="/" element={<Dashboard />} />
-
+        <Route path="/reports" element={<ReservationReport />} />
+        <Route path="/revenue-report" element={<RevenueReport />} />
+        <Route path="/staff-report" element={<StaffReport />} />
       </Route>
     </Routes>
   )
