cdd7b216c66711b07cc65b21c7c25de32d2fb28a
Oke Paul Olalekan <okepaul2006@gmail.com>
2026-06-15 23:58:01 +0100

    add login and forgot password pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom"
 import MainLayout from "./components/MainLayout"
 import Dashboard from "./pages/Dashboard"
-
+import Login from "./pages/Login"
+import ForgottenPassword from "./pages/ForgottenPassword"
 function App() {
   return (
     <Routes>
